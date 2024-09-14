50f9e30089ae268679f234877d187aa717f6fbcd
Pouria Jahandideh <pjsofts@gmail.com>
2024-09-14 14:20:30 +0330

    footer responsive

--- week10/hello-react/src/components/Footer.tsx
+++ week10/hello-react/src/components/Footer.tsx
@@ -1,5 +1,50 @@
 function Footer() {
-  return <div className="bg-[#4b0d49] text-white py-4 text-center">Footer</div>;
+  return (
+    <>
+      <div className="bg-[#4b0d49] text-white py-4 text-center flex flex-wrap gap-4">
+        <ul className="basis-60 flex-1">
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+        </ul>
+        <ul className=" basis-60 flex-1">
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+        </ul>
+        <ul className="  basis-60 flex-1">
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+        </ul>
+        <ul className=" basis-60 flex-1">
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+          <li>Downtown Dubai</li>
+          <li>Dubai Hills Estate</li>
+        </ul>
+      </div>
+
+      <ul className="flex gap-2">
+        <li className="basis-32">Privacy Policy</li>
+        <li className="flex-1 basis-32  ">Terms of use</li>
+        <li>Linkedin Icon</li>
+        <li>Instagram Icon</li>
+        <li>Twitter Icon</li>
+      </ul>
+    </>
+  );
 }
 
 export default Footer;
